2299f05e8d6b7ccf42629a69a6442e9e8932ce74
TheSkiDY <kk1999skidy@gmail.com>
2021-10-29 22:12:01 +0200

    Script tests, item indices, string fixes

---
+++ assembly/overworld_scripts/pallet_town.s
@@ -0,0 +1,12 @@
+.thumb
+.align 2
+
+.include "../xse_commands.s"
+.include "../xse_defines.s"
+.include "../asm_defines.s"
+
+.global EventScript_PalletTown_TechnologyGuy
+
+EventScript_PalletTown_TechnologyGuy:
+	msgbox gText_PalletTown_TechnologyGuy MSG_FACE
+	end
