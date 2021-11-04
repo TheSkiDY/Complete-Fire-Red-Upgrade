1a8116a3b199a208e3b7acc4c141020f0496cf6a
TheSkiDY <kk1999skidy@gmail.com>
2021-11-04 23:27:24 +0100

    Hisui Pokemon data

--- assembly/overworld_scripts/pallet_town.s
+++ assembly/overworld_scripts/pallet_town.s
@@ -8,8 +8,6 @@
 .global EventScript_PalletTown_TechnologyGuy
 
 EventScript_PalletTown_TechnologyGuy:
-	checkflag 0x200
-	if 0x1 _goto TechnologyGuy_Battle
 	lock 
 	loadpointer 0x0 gText_TechnologyGuyName
 	setvar 0x8000 0x1
@@ -18,10 +16,11 @@ EventScript_PalletTown_TechnologyGuy:
 	setvar 0x8003 0x2
 	callasm 0x8727CF9
 	faceplayer
-	givepokemon SPECIES_PIKACHU 42 0x0 0x0 0x0 0x0
-	givepokemon SPECIES_RILLABOOM 15 0x0 0x0 0x0 0x0
-	additem ITEM_DYNAMAX_CANDY 0x2
-	additem ITEM_DYNAMAX_BAND 0x1
+	givepokemon SPECIES_ZORUA_H 20 0x0 0x0 0x0 0x0
+	givepokemon SPECIES_ZOROARK_H 25 0x0 0x0 0x0 0x0
+	givepokemon SPECIES_BASCULEGION 20 0x0 0x0 0x0 0x0
+	givepokemon SPECIES_GROWLITHE_H 20 0x0 0x0 0x0 0x0
+	givepokemon SPECIES_BRAVIARY_H 20 0x0 0x0 0x0 0x0
 	msgbox gText_PalletTown_TechnologyGuy MSG_NORMAL
 	setflag 0x200
 	callasm 0x8727DCD
