596f8caaf99ebe66762adb5bed85cd534c9a5292
TheSkiDY <kk1999skidy@gmail.com>
2021-12-02 04:14:01 +0100

    ORAS trainer card setup

--- include/string_util.h
+++ include/string_util.h
@@ -38,6 +38,8 @@ s32 __attribute__((long_call)) StringCompareN(const u8 *str1, const u8 *str2, u3
 u8* __attribute__((long_call)) StringAppendN(u8 *dest, const u8 *src, u8 n);
 u8* __attribute__((long_call)) StringCopyPadded(u8 *dest, const u8 *src, u8 c, u16 n);
 
+void __attribute__((long_call)) ConvertInternationalString(u8 *s, u8 language);
+
 /*
 u8 *StringGetEnd10(u8 *str);
 u8 *StringCopy7(u8 *dest, const u8 *src);
--- src/trainer_card.c
+++ src/trainer_card.c
@@ -151,10 +151,18 @@ extern struct TrainerCardData *sTrainerCardDataPtr;
 
 extern const u8 gText_TrainerCardLevelCap[];
 
-static const u8 sTrainerCardTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_DARK_GREY, TEXT_COLOR_LIGHT_GREY};
+static const u8 sTrainerCardTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_WHITE, TEXT_COLOR_DARK_GREY};
 //static const u8 sTrainerCardStatColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_RED, TEXT_COLOR_LIGHT_RED};
-//static const u8 sTimeColonInvisibleTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_TRANSPARENT, TEXT_COLOR_TRANSPARENT};
+static const u8 sTimeColonInvisibleTextColors[] = {TEXT_COLOR_TRANSPARENT, TEXT_COLOR_TRANSPARENT, TEXT_COLOR_TRANSPARENT};
 static const u8 sTrainerCardFontIds[] = {0, 2, 0};
+
+static const u8 sTrainerCardTimeHoursXPositions[] = {0x65, 0x55};
+static const u8 sTrainerCardTimeHoursYPositions[] = {0x77, 0x67};
+static const u8 sTrainerCardTimeMinutesXPositions[] = {0x7C, 0x6C};
+static const u8 sTrainerCardTimeMinutesYPositions[] = {0x58, 0x59};
+static const u8 sTrainerCardFrontNameXPositions[] = {0x14, 0x10};
+static const u8 sTrainerCardFrontNameYPositions[] = {0x1D, 0x21};
+static const u8 *const sTimeColonTextColors[] = {sTrainerCardTextColors, sTimeColonInvisibleTextColors};
 
 #define gText_TrainerCardPokedex (const u8*) 0x8419CEF
 #define gText_TrainerCardNull (const u8*) 0x8419CF7
@@ -169,7 +177,7 @@ void PrintPokedexOnCard(void)
     {
     	levelCap = VarGet(VAR_LEVEL_CAP);
     	ConvertIntToDecimalStringN(buffer, levelCap, 0, 3);
-		x = -120 - 6 * StringLength(buffer);
+		x = -128 - 6 * StringLength(buffer);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 20, 72, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardLevelCap);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], x, 72, sTrainerCardTextColors, TEXT_SPEED_FF, buffer);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 138, 72, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardNull);
@@ -177,9 +185,76 @@ void PrintPokedexOnCard(void)
     else if (FlagGet(FLAG_SYS_POKEDEX_GET))
     {
         ConvertIntToDecimalStringN(buffer, sTrainerCardDataPtr->trainerCard.rse.caughtMonsCount, 0, 3);
-        x = -120 - 6 * StringLength(buffer);
+        x = -128 - 6 * StringLength(buffer);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 20, 72, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardPokedex);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], x, 72, sTrainerCardTextColors, TEXT_SPEED_FF, buffer);
         AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 138, 72, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardNull);
     }
+}
+
+#define gText_TrainerCardMoney (const u8*) 0x8419CE7
+#define gText_TrainerCardYen (const u8*) 0x8419CED
+void PrintMoneyOnCard(void)
+{
+    u8 buffer[10];
+    u8* txtPtr;
+    u8 x;
+
+    txtPtr = StringCopy(buffer, gText_TrainerCardYen);
+    ConvertIntToDecimalStringN(txtPtr, sTrainerCardDataPtr->trainerCard.rse.money, STR_CONV_MODE_LEFT_ALIGN, 6);
+    x = -130 - 6 * StringLength(buffer);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 20, 56, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardMoney);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], x, 56, sTrainerCardTextColors, TEXT_SPEED_FF, buffer);
+
+}
+
+
+#define gText_TrainerCardTime (const u8*) 0x8419CFD
+#define gText_Colon2 (const u8*) 0x8419CF8
+void PrintTimeOnCard(void)
+{
+    u16 hours;
+    u16 minutes;
+    u8 buffer[6];
+
+    hours = gSaveBlock2->playTimeHours;
+    minutes = gSaveBlock2->playTimeMinutes;
+    if (sTrainerCardDataPtr->isLink)
+    {
+        hours = sTrainerCardDataPtr->trainerCard.rse.playTimeHours;
+        minutes = sTrainerCardDataPtr->trainerCard.rse.playTimeMinutes;
+    }
+
+    if (hours > 999)
+        hours = 999;
+
+    if (minutes > 59)
+        minutes = 59;
+
+    FillWindowPixelRect(1, PIXEL_FILL(0), sTrainerCardTimeHoursXPositions[sTrainerCardDataPtr->cardType], sTrainerCardTimeMinutesYPositions[sTrainerCardDataPtr->cardType], 50, 12);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], 20, 88, sTrainerCardTextColors, TEXT_SPEED_FF, gText_TrainerCardTime);
+
+    ConvertIntToDecimalStringN(buffer, hours, STR_CONV_MODE_RIGHT_ALIGN, 3);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], sTrainerCardTimeHoursXPositions[sTrainerCardDataPtr->cardType],
+        sTrainerCardTimeMinutesYPositions[sTrainerCardDataPtr->cardType], sTrainerCardTextColors, TEXT_SPEED_FF, buffer);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], sTrainerCardTimeHoursYPositions[sTrainerCardDataPtr->cardType],
+        sTrainerCardTimeMinutesYPositions[sTrainerCardDataPtr->cardType], sTimeColonTextColors[sTrainerCardDataPtr->timeColonInvisible], TEXT_SPEED_FF, gText_Colon2);
+
+    ConvertIntToDecimalStringN(buffer, minutes, STR_CONV_MODE_LEADING_ZEROS, 2);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], sTrainerCardTimeMinutesXPositions[sTrainerCardDataPtr->cardType], sTrainerCardTimeMinutesYPositions[sTrainerCardDataPtr->cardType], sTrainerCardTextColors, TEXT_SPEED_FF, buffer);
+}
+
+
+#define gText_TrainerCardName (const u8*) 0x8419CDA
+void PrintNameOnCardFront(void)
+{
+    u8 buffer[2][32];
+    u8* txtPtr;
+
+    txtPtr = StringCopy(buffer[0], gText_TrainerCardName);
+    txtPtr = buffer[1];
+    StringCopy(txtPtr, sTrainerCardDataPtr->trainerCard.rse.playerName);
+    ConvertInternationalString(txtPtr, sTrainerCardDataPtr->language);
+    StringAppend(buffer[0], txtPtr);
+    AddTextPrinterParameterized3(1, sTrainerCardFontIds[1], sTrainerCardFrontNameXPositions[sTrainerCardDataPtr->cardType], sTrainerCardFrontNameYPositions[sTrainerCardDataPtr->cardType], sTrainerCardTextColors, TEXT_SPEED_FF, buffer[0]);
 }
