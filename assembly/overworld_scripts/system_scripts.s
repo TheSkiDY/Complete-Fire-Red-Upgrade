880ac4d71bbe6a048e6845ac38db43e30890eaf3
TheSkiDY <kk1999skidy@gmail.com>
2022-05-09 23:51:12 +0200

    New HMs system

--- assembly/overworld_scripts/system_scripts.s
+++ assembly/overworld_scripts/system_scripts.s
@@ -1017,4 +1017,79 @@ EventScript_HealingAnim:
 Movement_FaceDownAfterHealing:
 	.byte look_down, end_m
 
-	
+
+@;@@@@@@@@@@@@@@@@@@@@@@ Custom HMs @@@@@@@@@@@@@@@@
+
+.global EventScript_CutTree
+EventScript_CutTree:
+	lockall
+	special 0x10A
+	compare 0x8004 0x6
+	if 0x1 _goto EventScript_CutTree_Description
+	setanimation 0x0 0x8004
+	doanimation 0x2
+	waitstate
+	sound 0x79
+	applymovement LASTTALKED CutTree_Movement
+	waitmovement 0x0
+	checksound
+	hidesprite LASTTALKED
+	releaseall
+	end
+
+EventScript_CutTree_Description:
+	textcolor 0x2
+	msgbox gText_CantCutTree 0x6
+	releaseall
+	end
+
+CutTree_Movement:
+	.byte 0x69
+	.byte 0xFE
+
+.global EventScript_RockSmash
+EventScript_RockSmash:
+	lockall
+	special 0x10B
+	compare 0x8004 0x6
+	if 0x1 _goto EventScript_RockSmash_Description
+	setanimation 0x0 0x8004
+	doanimation 0x25
+	waitstate
+	sound 0x7C
+	applymovement LASTTALKED RockSmash_Movement
+	waitmovement 0x0
+	checksound
+	hidesprite LASTTALKED
+	releaseall
+	end
+
+EventScript_RockSmash_Description:
+	textcolor 0x2
+	msgbox gText_CantSmashRock 0x6
+	releaseall
+	end
+
+RockSmash_Movement:
+	.byte 0x68
+	.byte 0xFE
+
+.global EventScript_Strength
+EventScript_Strength:
+	lockall
+	checkflag 0x805
+	if 0x1 _goto EventScript_Strength_AlreadyPushed
+	special 0x10C
+	compare 0x8004 0x6
+	if 0x1 _goto EventScript_Strength_Description
+	setflag 0x805
+	msgbox gText_StrengthStartPushing 0x3
+	end
+
+EventScript_Strength_AlreadyPushed:
+	msgbox gText_StrengthBoulderAlreadyPushed 0x3
+	end
+
+EventScript_Strength_Description:
+	msgbox gText_CantPushStrengthBoulder 0x3
+	end
